458852c52cb164f49d75017d2927b6465e90dfdf
Mikhail Zuev <mikhailneue@gmail.com>
2026-09-02 23:16:06 +0300

    add constants

--- src/prefixselection/constants/constants.py
+++ src/prefixselection/constants/constants.py
@@ -1,9 +1,27 @@
-from enum import Enum
+MESSAGES = {
+    'EN': {
+        'enter_model_to_download': 'Add model to the ensemble',
+        'generate': 'Make generation in response',
+        'model_prompt': 'Enter the model name from Hugging Face.',
+        'wrong_input': 'Such a model does not exist on Hugging Face. Enter the correct name.',
+        'downloading': 'Downloading...',
+        'chosen': 'The model {model_name} is installed. The number of models in the ensemble: {number_of_models}',
+        'menu': 'Choose command',
+        'quit': 'Quit',
+    },
+    'RU': {
+        'enter_model_to_download': 'Добавить модель к ансамблю',
+        'generate': 'Выполнить генерацию в ответ на ввод',
+        'model_prompt': 'Введите название модели с Hugging Face',
+        'wrong_input': 'Такой модели на hugging face не существует. Введите корректное название',
+        'downloading': 'Скачивание началось...',
+        'chosen': 'Модель {model_name} установлена. Число моделей в ансамбле: {number_of_models}',
+        'menu': 'Выберите команду',
+        'quit': 'Выход',
+    },
+}
 
-
-class Lang(Enum):
-    RU='русский 🇷🇺'
-    EN='english 🇬🇧'
-
-MESSAGES={'EN': {'download_model': 'Enter the name of the model you want to download from Hugging Face'},
-          'RU': {'download_model': 'Введите название модели, которую хотите скачать с Hugging Face'}}
+MENU = [
+    ("enter_model_to_download", "load-model"),
+    ("generate", "generate"),
+]
